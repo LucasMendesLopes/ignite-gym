737314a0904d3c3f7d410326a81ec19195873317
lucas.lopes <lucasmendeslopes.pessoal@gmail.com>
2024-11-05 14:31:13 -0300

    feat: muscle group tabs

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import { Button } from "./button";
+import { Group } from "./group";
 import { HomeHeader } from "./home-header";
 import { Input } from "./input";
 import { Loading } from "./loading";
 
-export { Button, HomeHeader, Input, Loading };
+export { Button, Group, HomeHeader, Input, Loading };
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,33 @@
-import { HomeHeader } from "@components";
+import { Group, HomeHeader } from "@components";
 import { VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+    const [groups, setGroups] = useState(["peito", "perna", "ombro", "costas", "braço"])
+    const [selectedGroup, setSelectedGroup] = useState("peito")
+
     return (
         <VStack flex={1}>
             <HomeHeader />
+
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <Group
+                        title={item}
+                        onPress={() => setSelectedGroup(item)}
+                        isActive={selectedGroup === item}
+                    />
+                )}
+                horizontal
+                contentContainerStyle={{
+                    paddingHorizontal: 32
+                }}
+                style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+                showsHorizontalScrollIndicator={false}
+            />
         </VStack>
     )
 }
